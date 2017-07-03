21e37f6481e4ee0c337108f9ce849397ff873e62
Anthony Wratten <awratten1@gmail.com>
2017-07-04 08:02:22 +1000

    Add EMA based on ocirneaquilina's reccomendations.

--- part 3/botindicators.py
+++ part 3/botindicators.py
@@ -9,3 +9,16 @@ class BotIndicators(object):
 	def momentum (self, dataPoints, period=14):
 		if (len(dataPoints) > period -1):
 			return dataPoints[-1] * 100 / dataPoints[-period]
+
+	def EMA(self, dataPoints, period, position=None, previous_ema=None):
+		"""https://www.oanda.com/forex-trading/learn/forex-indicators/exponential-moving-average"""
+		if len(dataPoints) < period + 2:
+			return None
+		c = 2 / float(period + 1)
+		if not previous_ema:
+			return EMA(dataPoints, period, period, movingAverage(dataPoints[-period*2 + 1:-period + 1], period))
+		else:
+			current_ema = (c * dataPoints[-position]) + ((1 - c) * previous_ema)
+			if position > 0:
+				return EMA(dataPoints, period, position - 1, current_ema)
+		return previous_ema
